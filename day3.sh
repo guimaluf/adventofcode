dc59c00335ce3afda4a0daf33288a7aa035aa09a
Guilherme Maluf Balzana <guilherme.balzana@canonical.com>
2022-12-03 10:46:19 +0100

    Part1 Day3

---
+++ day3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+declare -A priority
+
+for j in {a..z} {A..Z}; do
+  priority[$j]=$((++i))
+done
+
+while read rucksack; do
+  middle=$((${#rucksack}/2))
+
+  rcks1=${rucksack::${middle}}
+  rcks2=${rucksack:${middle}}
+
+  item=${rcks2//[^${rcks1}]/}
+  printf "%s %s\n%s %d\n\n" ${rcks1} ${rcks2} ${item::1} ${priority[${item::1}]}
+
+  sum=$((sum + ${priority[${item::1}]}))
+done
+
+echo $sum
