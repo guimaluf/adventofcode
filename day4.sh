adccbcb8c897297e3fa99762402cf7a093702e05
Guilherme Maluf Balzana <guilherme.balzana@canonical.com>
2022-12-04 12:16:45 +0100

    Day4 Part2

--- day4.sh
+++ day4.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-declare -i sum=0
+declare -i sum=0 overlap=0
 while IFS=, read elf1 elf2 ; do
+  # Part1
   sections1="$(eval echo {${elf1/-/..}})"
   sections2="$(eval echo {${elf2/-/..}})"
 
@@ -9,6 +10,19 @@ while IFS=, read elf1 elf2 ; do
      [[ " ${sections2} " =~ " ${sections1} " ]] ; then
        ((sum++))
   fi
+
+  # Part2
+  unset IFS
+  s1=(${sections1})
+  s2=(${sections2})
+
+  contains=$(comm -12 \
+    <(printf "%s\n" "${s1[@]}" | sort) \
+    <(printf "%s\n" "${s2[@]}" | sort))
+  if [[ -n ${contains} ]]; then
+    ((overlap++))
+  fi
 done
 
 echo $sum
+echo $overlap
